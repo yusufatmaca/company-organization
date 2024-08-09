a6d286327153990d68932693d58c2d5c29117e77
Yusuf Atmaca <yusufatmaca@std.iyte.edu.tr>
2024-08-09 19:41:08 +0300

    swagger integration

--- src/main/java/com/deltasmarttech/companyorganization/config/SecurityConfig.java
+++ src/main/java/com/deltasmarttech/companyorganization/config/SecurityConfig.java
@@ -35,7 +35,19 @@ public class SecurityConfig{
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize)->authorize
-                        .requestMatchers("/api/v1/auth/**")
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html"
+                        )
                         .permitAll()
 
                         //MANAGER ENDPOINTS
