ae3725e5ddaf73b0c0f3cc022bafa5a658ff4234
Yusuf Atmaca <yusufatmaca@std.iyte.edu.tr>
2024-08-09 19:45:49 +0300

    cors integrity

--- src/main/java/com/deltasmarttech/companyorganization/controllers/DepartmentHierarchyController.java
+++ src/main/java/com/deltasmarttech/companyorganization/controllers/DepartmentHierarchyController.java
@@ -1,8 +1,10 @@
-package com.deltasmarttech.companyorganization.controller;
+package com.deltasmarttech.companyorganization.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"*"}, maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class DepartmentHierarchyController {
--- src/main/java/com/deltasmarttech/companyorganization/controllers/ManagerController.java
+++ src/main/java/com/deltasmarttech/companyorganization/controllers/ManagerController.java
@@ -3,6 +3,7 @@ package com.deltasmarttech.companyorganization.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = {"*"}, maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/manager")
 public class ManagerController {
